f50afaa076ccd9a7707d57d4d5a60c16c395add8
HenryVF <henryvelezmorofranco@gmail.com>
2022-06-25 01:38:38 -0500

    parte valoracion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {FormularioComponent} from './formulario/formulario.component';
+import {ValoracionListComponent} from './admin/valoracion-list/valoracion-list.component';
 import {ValoracionComponent} from './valoracion/valoracion.component';
+import {FormularioListComponent} from './admin/formulario-list/formulario-list.component';
 import {RegistroComponent} from './registro/registro.component';
 import {InicioComponent} from './inicio/inicio.component';
 import { TipsComponent } from './tips/tips.component';
 
 const routes: Routes = [
+  {
+    path: 'valoraciones',
+    component: ValoracionListComponent
+  },
+  {
+    path: 'formularios',
+    component: FormularioListComponent
+  },
   {
     path: 'inicio',
     component: InicioComponent
---
+++ src/app/entities/valoracion.ts
@@ -0,0 +1,8 @@
+export interface Valoracion{
+    id: number;
+    nombre: string;
+    apellido: string;
+    email: string;
+    comentario: string;
+    valor: number
+}
